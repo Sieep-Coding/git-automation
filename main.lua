76dab1637ee50efa9b7f0d55197e4f7544c27049
Nick <nstamb6@gmail.com>
2024-09-16 21:30:36 -0400

    calc

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-function add(x, y)
+function add(x,y)
   print(x+y)
 end
 
@@ -6,7 +6,18 @@ function subtract(x,y)
   print(x-y)
 end
 
-local tble = { [1] = subtract(10,3), [2] = add(3,5),}
+function divide(x,y)
+  print(x/y)
+end
+
+function multiply(x,y)
+  print(x*y)
+end
+
+local tble = { [1] = subtract(10,3), [2] = add(3,5),
+               [3] = divide(10,2), [4] = multiply(2,5)
+}
+
 
 local func = tble[1]
 if (func) then
