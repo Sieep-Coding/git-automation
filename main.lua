f4d58f2d62ff51d9b3aace52cd48e7ef9d9ad066
Nick <nstamb6@gmail.com>
2024-09-16 21:24:35 -0400

    making switch statementgit add .

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
-fonct = {name = 'Hayden'}
-
-function wedgie()
-  print('YANK THAT SHIT')
+function add(x, y)
+  print(x+y)
 end
 
-if fonct.name == 'Hayden' then
-  wedgie()
+function subtract(x,y)
+  print(x-y)
+end
+
+local tble = { [1] = subtract(10,3), [2] = add(3,5),}
+
+local func = tble[1]
+if (func) then
+  func()
 else
-  print("Not Fonct, do not wedgie")
+  print 'Terminated'
 end
+
+
+
